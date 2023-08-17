1eaf9f825aba44160dd623ffb27c4ff1c7f9fd05
linckon <linckon@163.com>
2023-08-17 10:00:12 +0800

    test

---
+++ xdclass-common/src/main/java/net/comeriver/domain/Video.java
@@ -0,0 +1,95 @@
+package net.comeriver.domain;
+
+import java.util.Date;
+
+public class Video {
+    private Integer id;
+    private String title;
+    private String summary;
+    private String coverImg;
+    private Integer price;
+    private Date createTime;
+    private Double point;
+    //测试服务
+    private String serveInfo;
+
+    public Video() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getCoverImg() {
+        return coverImg;
+    }
+
+    public void setCoverImg(String coverImg) {
+        this.coverImg = coverImg;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Double getPoint() {
+        return point;
+    }
+
+    public void setPoint(Double point) {
+        this.point = point;
+    }
+
+    public String getServeInfo() {
+        return serveInfo;
+    }
+
+    public void setServeInfo(String serveInfo) {
+        this.serveInfo = serveInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", summary='" + summary + '\'' +
+                ", coverImg='" + coverImg + '\'' +
+                ", price=" + price +
+                ", createTime=" + createTime +
+                ", point=" + point +
+                '}';
+    }
+}
